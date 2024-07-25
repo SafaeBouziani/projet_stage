67f069b58d06b3f81a48fdcf83552d828c8829ac
unknown <hiba.chaoui@uit.ac.ma>
2024-07-25 12:50:15 +0200

    update seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $this->call([
+            RolesSeeder::class,
+            UserSeeder::class,
 
+        ]);
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
